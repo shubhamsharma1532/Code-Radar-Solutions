e6f6ebe69232433b88bd6ebc3fa47ade1a000cf0
shubahm sharma <shubham180905@gmail.com>
2025-02-11 21:52:50 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -14,7 +14,7 @@ int main(){
     }
     else if(ch == '/'){
         if (y!=0){
-            printf("%d, x/b");
+            printf("%d," x/y);
         }
         else{
             printf("error");
