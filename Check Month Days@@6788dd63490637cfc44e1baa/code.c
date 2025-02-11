fb681fc8e78fa17d91c913a28156b2af6cd6f56e
shubahm sharma <shubham180905@gmail.com>
2025-02-11 20:25:53 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,42 +1,81 @@
+// #include <stdio.h>
+// int main(){
+//     int x;
+//     scanf("%d",&x);
+//     if(x=='1'){
+//         printf("31");
+//     }
+//     else if(x=='2'){
+//         printf("28");
+//     }
+//     else if(x=='3'){
+//         printf("31");
+//     }
+//     else if(x=='4'){
+//         printf("30");
+//     }
+//     else if(x=='5'){
+//         printf("31");
+//     }
+//     else if(x=='6'){
+//         printf("30");
+//     }
+//     else if(x=='7'){
+//         printf("31");
+//     }
+//     else if(x=='8'){
+//         printf("31");
+//     }
+//     else if(x=='9'){
+//         printf("30");
+//     }
+//     else if(x=='10'){
+//         printf("31");
+//     }
+//     else if(x=='11'){
+//         printf("30");
+//     }
+//     else if(x=='12'){
+//         printf("31");
+//     }
+//     return 0;
+// }
+
+
+
+
+
 #include <stdio.h>
-int main(){
+
+int main() {
     int x;
-    scanf("%d",&x);
-    if(x=='1'){
+    scanf("%d", &x);
+
+    if (x == 1) {
         printf("31");
-    }
-    else if(x=='2'){
+    } else if (x == 2) {
         printf("28");
-    }
-    else if(x=='3'){
+    } else if (x == 3) {
         printf("31");
-    }
-    else if(x=='4'){
+    } else if (x == 4) {
         printf("30");
-    }
-    else if(x=='5'){
+    } else if (x == 5) {
         printf("31");
-    }
-    else if(x=='6'){
+    } else if (x == 6) {
         printf("30");
-    }
-    else if(x=='7'){
+    } else if (x == 7) {
         printf("31");
-    }
-    else if(x=='8'){
+    } else if (x == 8) {
         printf("31");
-    }
-    else if(x=='9'){
+    } else if (x == 9) {
         printf("30");
-    }
-    else if(x=='10'){
+    } else if (x == 10) {
         printf("31");
-    }
-    else if(x=='11'){
+    } else if (x == 11) {
         printf("30");
-    }
-    else if(x=='12'){
+    } else if (x == 12) {
         printf("31");
     }
+
     return 0;
 }
