84d26305e3e6b78a8c3f956dd2404ca99a13acd1
shubahm sharma <shubham180905@gmail.com>
2025-03-21 21:51:51 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -1,42 +1,42 @@
-// #include <stdio.h>
-
-// int main() {
-//     int n;
-//     scanf("%d", &n);
-//     for(int i=1; i<n; i++){
-//         for(int j=1; j<n; j++){
-//             if(i==1 || i==n || j==1 || j==n){
-//                 printf("*");
-//             }else{
-//                 printf(" ");
-//             }
-//         }
-//         printf("\n");
-
-//     }
-//     return 0;
-// }
-
 #include <stdio.h>
 
 int main() {
     int n;
     scanf("%d", &n);
-
-    for (int i = 1; i <= n; i++) {  // Loop for rows
-        for (int j = 1; j <= n; j++) {  // Loop for columns
-            // Print '*' if it's a border position, otherwise print space
-            if (i == 1 || i == n || j == 1 || j == n) {
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==1 || i==n || j==1 || j==n){
                 printf("*");
-            } else {
+            }else{
                 printf(" ");
             }
         }
-        printf("\n");  // Move to the next line
+        printf("\n");
+
     }
-    
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++) {  // Loop for rows
+//         for (int j = 1; j <= n; j++) {  // Loop for columns
+//             // Print '*' if it's a border position, otherwise print space
+//             if (i == 1 || i == n || j == 1 || j == n) {
+//                 printf("*");
+//             } else {
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");  // Move to the next line
+//     }
+    
+//     return 0;
+// }
 
 
 
